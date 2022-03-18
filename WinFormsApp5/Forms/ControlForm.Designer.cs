3bf4864331b8fba5f7d97c6f4cae564984565f13
qkrwjdrb <miolionpjg@gmail.com>
2022-03-18 18:07:55 +0900

    클래스 공부

--- WinFormsApp5/Forms/ControlForm.Designer.cs
+++ WinFormsApp5/Forms/ControlForm.Designer.cs
@@ -62,6 +62,14 @@ namespace WinFormsApp5.Forms
             this.상태 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
             this.groupBox3.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox4.SuspendLayout();
@@ -232,7 +240,7 @@ namespace WinFormsApp5.Forms
             this.groupBox1.Controls.Add(this.checkBox1);
             this.groupBox1.Controls.Add(this.textBox3);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Location = new System.Drawing.Point(12, 302);
+            this.groupBox1.Location = new System.Drawing.Point(277, 127);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(272, 63);
             this.groupBox1.TabIndex = 43;
@@ -282,7 +290,7 @@ namespace WinFormsApp5.Forms
             this.groupBox4.Controls.Add(this.상태);
             this.groupBox4.Controls.Add(this.label8);
             this.groupBox4.Controls.Add(this.label7);
-            this.groupBox4.Location = new System.Drawing.Point(12, 371);
+            this.groupBox4.Location = new System.Drawing.Point(856, 127);
             this.groupBox4.Name = "groupBox4";
             this.groupBox4.Size = new System.Drawing.Size(177, 169);
             this.groupBox4.TabIndex = 44;
@@ -414,6 +422,86 @@ namespace WinFormsApp5.Forms
             this.label7.TabIndex = 41;
             this.label7.Text = "상태";
             // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(283, 222);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 23);
+            this.button9.TabIndex = 1;
+            this.button9.Text = "좌회전";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(389, 222);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(100, 23);
+            this.button10.TabIndex = 1;
+            this.button10.Text = "좌회전";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(495, 222);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(100, 23);
+            this.button11.TabIndex = 1;
+            this.button11.Text = "좌회전";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(601, 222);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(100, 23);
+            this.button12.TabIndex = 1;
+            this.button12.Text = "좌회전";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(283, 267);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(100, 23);
+            this.button13.TabIndex = 1;
+            this.button13.Text = "좌회전";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(389, 267);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(100, 23);
+            this.button14.TabIndex = 1;
+            this.button14.Text = "좌회전";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(495, 267);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(100, 23);
+            this.button15.TabIndex = 1;
+            this.button15.Text = "좌회전";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(601, 267);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(100, 23);
+            this.button16.TabIndex = 1;
+            this.button16.Text = "좌회전";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button2_Click);
+            // 
             // ControlForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -421,6 +509,14 @@ namespace WinFormsApp5.Forms
             this.ClientSize = new System.Drawing.Size(1045, 748);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.deviceBox);
@@ -472,5 +568,13 @@ namespace WinFormsApp5.Forms
         public System.Windows.Forms.Label 상태;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
     }
 }
